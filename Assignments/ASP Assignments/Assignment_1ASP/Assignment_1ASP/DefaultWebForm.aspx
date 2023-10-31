61b5091a577b3039ce5e307bb15d72d6c684c741
Zainab Belal <zainabb@infinite.com>
2023-10-31 22:40:15 +0530

    ASPAssignment

---
+++ Assignments/ASP Assignments/Assignment_1ASP/Assignment_1ASP/DefaultWebForm.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DefaultWebForm.aspx.cs" Inherits="Assignment_1ASP.DefaultWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <h2><i>Welcome to Stationary Store!!!!</i></h2>
+        <asp:DropDownList ID="listItems" runat="server" AutoPostBack="true" OnSelectedIndexChanged="listItems_SelectedIndexChanged">
+            <asp:ListItem Text="Select an item" Value="" />
+            <asp:ListItem Text="Notebook" Value="Notebook.jpg" />
+            <asp:ListItem Text="Pen" Value="Pen.jpg" />
+            <asp:ListItem Text="Color Pencils" Value="Colorpencils.jpg" />
+            <asp:ListItem Text="Eraser" Value="Eraser.jpg" />
+            <asp:ListItem Text="Whitener" Value="Whitener.jpg" />
+            <asp:ListItem Text="Stapler" Value="Stapler.webp" />
+
+        </asp:DropDownList>
+
+        <br /><br />
+
+        <asp:Image ID="imageItem" runat="server" Width="200px" Height="150px" />
+                    
+        <br /><br />
+
+         <asp:Button ID="ShowDetail" runat="server" Text="Show Details" OnClick="getItemDetails" />
+        <br /><br />
+        <asp:Label ID="ItemDetail" runat="server" Text=""></asp:Label>
+
+      
+        <br /><br />
+        <asp:Button ID="ShowCost" runat="server" Text="Show Cost" OnClick="getItemCost" />
+
+        <br /><br />
+
+        <asp:Label ID="ItemCost" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Assignments/ASP Assignments/Assignment_1ASP/Assignment_1ASP/DefaultWebForm.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Assignment_1ASP
+{
+    using System;
+
+    public partial class DefaultWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+               
+                imageItem.ImageUrl = "Imgs/Stationery.jpg";
+            }
+        }
+
+        protected void listItems_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+
+            string selectedItem = listItems.SelectedValue;
+            imageItem.ImageUrl = $"Imgs/{selectedItem}";
+
+            
+        }
+        protected void getItemCost(object sender, EventArgs e)
+        {
+            string selectedItem =listItems.SelectedValue;
+
+           
+            var Costs = new Dictionary<string, decimal>
+            {
+                {"Notebook.jpg", 200.00m },
+                {"Pen.jpg", 50.00m },
+                {"Colorpencils.jpg", 100.00m },
+                {"Eraser.jpg", 12.00m },
+                {"Whitener.jpg",60.00m},
+                //{"Stapler.webp",},
+            };
+            
+            if (Costs.ContainsKey(selectedItem))
+            {
+                ItemCost.Text = $"Cost: Rs {Costs[selectedItem]}";
+            }
+            else
+            {
+                ItemCost.Text = "Sorry for the Incovinience......Item currently out of Stock.........";
+            }
+        }
+        protected void getItemDetails(object sender, EventArgs e)
+        {
+            string selectedItem = listItems.SelectedValue;
+
+             
+            var Details = new Dictionary<string, string>
+            {
+                {"Notebook.jpg","Notebooks serve as a medium to remeber things.With these notebook you get premium quality pages and more pages in less cost!!! " },
+                {"Pen.jpg","Use this pen to dot down your favourite memories!!!" },
+                {"Colorpencils.jpg","Add Colours to your life and drawings using these amazing color pencils!!!" },
+                {"Eraser.jpg","Make mistakes....Erase them....Correct your mistakes!!!" },
+                {"Whitener.jpg","Use whitener to remove mistakes which can not be erased!!!"},
+                //{"Stapler.webp",},
+            };
+
+            if (Details.ContainsKey(selectedItem))
+            {
+                ItemCost.Text = $"{Details[selectedItem]}";
+            }
+            else
+            {
+                ItemCost.Text = "Sorry for the Incovinience......Item currently out of Stock.........";
+            }
+        }
+
+
+
+
+        //public void getpricebyname(string itemname)
+        //{
+        //    SqlConnection con = new SqlConnection("Data Source=ICS-LT-4KBDR73;" +
+        //       "Initial Catalog=INFINITE_DB;Integrated Security=true");
+        //    SqlDataAdapter sda = new SqlDataAdapter("getpricebyname", con);
+        //    sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+        //    SqlParameter sqlp = new SqlParameter();
+        //    sqlp.ParameterName = "@itemname";
+        //    sqlp.Value = itemname;
+        //    sda.SelectCommand.Parameters.Add(sqlp);
+        //}
+
+        
+
+
+
+    }
+
+}
